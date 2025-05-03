a0024f4737d7762f1eaff97657f990431844c62a
kauan <kauancavalcante8p@gmail.com>
2025-05-03 14:14:26 -0300

    entendendo peculiaridades do spring data jpa

--- src/main/java/br/com/kauan/demo/modules/candidato/CandidatoRepository.java
+++ src/main/java/br/com/kauan/demo/modules/candidato/CandidatoRepository.java
@@ -3,7 +3,13 @@ package br.com.kauan.demo.modules.candidato;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
-
+                                                           //Entidade       //tipo de Id dela
 public interface CandidatoRepository extends JpaRepository<CandidatoEntity, UUID> {
 
+    /*
+    * no Spring Data Jpa, conseguimos criar alguns metedos de forma automatizada utilizando
+    * algumas palavras como atalho.
+    * Ao escrever findBy o spring irá fazer um select (Or, And tbm são palavras automatizadas)
+    */
+    CandidatoEntity findByUsernameOrEmail(String username, String email);
 }
