49276f10ac226842ca76717320318387e455316b
kauan <kauancavalcante8p@gmail.com>
2025-05-07 14:57:46 -0300

    corrijindo erro de referenciação de tabelas

--- src/main/java/br/com/kauan/demo/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/kauan/demo/modules/company/entities/JobEntity.java
@@ -24,13 +24,17 @@ public class JobEntity {
     entender qual o nivel da relação entre as tabelas, no caso o relacionamento se dá, da tabela que está sendo criada
     para a tabela em q será associada
     nesse caso em questão: Muitos Jobs para uma Company
+    Não é estritamente necessário ter a referência private CompanyEntity company. Ela é opcional, mas muito útil dependendo
+    de como você quer trabalhar com os dados.
+    Quando é necessário ter a referência (company):
+    Quando você quer acessar diretamente os dados da empresa a partir do Job, sem precisar fazer outra consulta manual.
     */
     @ManyToOne
-    @JoinColumn(name = "company_id") //ao inserir um job, faz com que se verfique a existencia do id da compania
+    @JoinColumn(name = "company_id", insertable = false, updatable = false) //ao inserir um job, faz com que se verfique a existencia do id da compania
     private CompanyEntity company;
 
-    @Column(name = "company_id") //faz dessa variavel representar a coluna respectiva
-    private UUID companyId;      //não colocar resulta em variavel e coluna terem o mesmo nome
+    @Column(name = "company_id") //campo que realmente vai ser escrito.
+    private UUID companyId;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
